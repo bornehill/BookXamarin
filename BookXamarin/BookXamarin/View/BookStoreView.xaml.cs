34f6999cc070af8c4e4c7648baeb3301634be234
bornehill <ali.escamilla.navarro@gmail.com>
2020-01-21 21:13:12 -0600

    Add BookStoreView

---
+++ BookXamarin/BookXamarin/View/BookStoreView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using BookXamarin.Service;
+using BookXamarin.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BookXamarin.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BookStoreView : ContentPage
+    {
+        public BookViewModel books;
+
+        public BookStoreView()
+        {
+            InitializeComponent();
+            var ser = new BookService();
+            books = new BookViewModel();
+            books.Books = ser.GetAllBook();
+            BindingContext = books;
+        }
+    }
+}
